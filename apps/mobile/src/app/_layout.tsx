883bda1c0f2a9f6c3638e7ca1ed7fdfef6183725
Elliot Datsomor <datsomorelliot56@gmail.com>
2026-07-15 23:22:48 +0000

    updated the layout

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,14 +1,24 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { GlobalSafetyCheckInProvider } from "../components/GlobalSafetyCheckInProvider";
+import { COLORS } from "../constants/theme";
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-        <StatusBar style="dark" />
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={COLORS.primary}
+          translucent={false}
+        />
+
+        <GlobalSafetyCheckInProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </GlobalSafetyCheckInProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
